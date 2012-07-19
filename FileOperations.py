1c7b9d27604c2a893959e4f4910630a132a5622c
Chase Stubblefield <chasestubblefield@gmail.com>
2012-07-18 22:10:03 -0700

    Refactor

--- FileOperations.py
+++ FileOperations.py
@@ -1,36 +1,45 @@
 import sublime, sublime_plugin
+import shutil
 import os
-import functools
-import send2trash
 
 class RenameFileCommand(sublime_plugin.WindowCommand):
 
   def run(self):
-    branch, leaf = os.path.split(self.window.active_view().file_name())
-    v = self.window.show_input_panel("New Name:", leaf, functools.partial(self.__on_name_given, branch, leaf), None, None)
+    ask_for_name_relative_to_active_view(self.window, self.rename_and_retarget)
 
-    # Highlight name and not extension
-    name, ext = os.path.splitext(leaf)
-    v.sel().clear()
-    v.sel().add(sublime.Region(0, len(name)))
-
-  def __on_name_given(self, branch, old_leaf, new_leaf):
-    old = os.path.join(branch, old_leaf)
-    new = os.path.join(branch, new_leaf)
+  def rename_and_retarget(self, src, dest):
     try:
-        os.rename(old, new)
-        v = self.window.find_open_file(old)
-        if v:
-            v.retarget(new)
+      os.rename(src, dest)
+      self.window.find_open_file(src).retarget(dest)
     except:
-        sublime.status_message("Unable to rename")
+      sublime.status_message("Unable to rename")
 
 class DuplicateFileCommand(sublime_plugin.WindowCommand):
 
   def run(self):
-    pass
+    ask_for_name_relative_to_active_view(self.window, self.copy_and_open_new_buffer)
 
-class DeleteFileCommand(sublime_plugin.WindowCommand):
+  def copy_and_open_new_buffer(self, src, dest):
+    try:
+      shutil.copy(src, dest)
+      self.window.open_file(dest)
+    except:
+      sublime.status_message("Unable to copy")
 
-  def run(self):
-    send2trash.send2trash(self.window.active_view().file_name())
+
+def ask_for_name_relative_to_active_view(window, on_done):
+  old_path = window.active_view().file_name()
+  branch, leaf = os.path.split(old_path)
+
+  def on_input_given(new_leaf):
+    new_path = os.path.join(branch, new_leaf)
+    if old_path != new_path:
+      on_done(old_path, new_path)
+
+  # Show input panel with basename of current view
+  v = window.show_input_panel("New Name:", leaf, on_input_given, None, None)
+
+  # Highlight name and not extension
+  name, ext = os.path.splitext(leaf)
+  v.sel().clear()
+  v.sel().add(sublime.Region(0, len(name)))
